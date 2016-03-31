07368e2dfa9dd0bb2fef36cc3dcab4f45f56dd17
Paco Gorina <fgorina@gmail.com>
2016-03-31 10:54:12 +0200

    Sustituit el delegate de Dashboard a RunningDashboard

--- 9BMetrics/BLEDeviceSelector.swift
+++ 9BMetrics/BLEDeviceSelector.swift
@@ -27,7 +27,7 @@ class BLEDeviceSelector: UIViewController {
     @IBOutlet weak var tableView : UITableView?
     
     var devices : [CBPeripheral] = [CBPeripheral] ()
-    weak var delegate : BLENinebotDashboard?
+    weak var delegate : BLERunningDashboard?
     
     
     func clearDevices(){
