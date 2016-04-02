23db3404fa083ff51fb24ce404af2826557f885e
Paco Gorina <fgorina@gmail.com>
2016-04-02 15:22:51 +0200

    Added Bridging Header for minizip

---
+++ 9BMetrics-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "minizip.h"
+#import "unzip.h"
+#import "crypt.h"
+#import "ioapi.h"
