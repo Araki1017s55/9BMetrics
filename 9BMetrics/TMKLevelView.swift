0c59322437e8f522d4ebf689f8db86e77c8b016f
Paco Gorina <fgorina@gmail.com>
2016-04-22 13:28:28 +0200

    Un indicador analògic per valors de angles al voltant de la horitzintalitat L'us a 9BMetrics es el següent :
    	1.- La linia verda indica ma mitja o el valor
    	2.- Les agulles vermelles indiquen els mínims i màxims
    	3.- Els números indiquen el valor, el mínim i el màxim

---
+++ 9BMetrics/TMKLevelView.swift
@@ -0,0 +1,232 @@
+//
+//  TMKLevelView.swift
+//  9BMetrics
+//
+//  Created by Francisco Gorina Vanrell on 22/4/16.
+//  Copyright © 2016 Paco Gorina. All rights reserved.
+//
+
+import UIKit
+
+class TMKLevelView: UIView {
+    
+    
+    var scale : Double    = 1.0  // Number to scale angles
+    var value : Double    = 0.0// Normal angle (green)
+    var minValue : Double = 30.0   // Minimum angle (red)
+    var maxValue : Double = -30.0   // Maximum value (red)
+    
+    
+    let startEndLength : CGFloat = 0.1   // Percentatge del radi
+    let sphereWidth : CGFloat = 1.0
+    let sphereColor = UIColor.yellowColor()
+    let cursorWidth : CGFloat = 1.0
+    let cursorColor = UIColor.redColor()
+    let cursorSize : CGFloat = 1.0
+    
+    var valueLabel : UILabel = UILabel()    // value center down
+    var minLabel : UILabel = UILabel()  // Minimum value left
+    var maxLabel : UILabel = UILabel()  // Maximum value right
+    
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setup()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setup()
+    }
+    
+    func setup(){
+        let r : CGFloat = (min(self.bounds.width, self.bounds.height) / 2.0)-1.0
+        
+        valueLabel.text = String(format: "%0.1f", value)
+        valueLabel.textAlignment = .Center
+        valueLabel.font = UIFont.systemFontOfSize(r / 4.0)
+        valueLabel.textColor = UIColor.whiteColor()
+        valueLabel.backgroundColor = UIColor.clearColor()
+        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(valueLabel)
+        
+        var c1 = NSLayoutConstraint(item: valueLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: -(r/2.0))
+        
+        var c2 = NSLayoutConstraint(item: valueLabel, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1.0, constant: 0.0 )
+        
+        
+        self.addConstraint(c1)
+        self.addConstraint(c2)
+        
+        minLabel.text = String(format: "%0.1f", minValue)
+        minLabel.textAlignment = .Center
+        minLabel.font = UIFont.systemFontOfSize(r / 4.0)
+        minLabel.textColor = UIColor.whiteColor()
+        minLabel.backgroundColor = UIColor.clearColor()
+        minLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(minLabel)
+        
+        c1 = NSLayoutConstraint(item: minLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+        
+        c2 = NSLayoutConstraint(item: minLabel, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1.0, constant: +( r / 2.0) )
+        
+        
+        self.addConstraint(c1)
+        self.addConstraint(c2)
+        
+        // max values
+        
+        maxLabel.text = String(format: "%0.1f", maxValue)
+        maxLabel.textAlignment = .Center
+        maxLabel.font = UIFont.systemFontOfSize(r / 4.0)
+        maxLabel.textColor = UIColor.whiteColor()
+        maxLabel.backgroundColor = UIColor.clearColor()
+        maxLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(maxLabel)
+        
+        c1 = NSLayoutConstraint(item: maxLabel, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1.0, constant: 0.0)
+        
+        c2 = NSLayoutConstraint(item: maxLabel, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1.0, constant: -( r / 2.0) )
+        
+        
+        self.addConstraint(c1)
+        self.addConstraint(c2)
+        
+    }
+    
+    func d2R(degs : Double ) -> CGFloat{
+        return CGFloat(degs / 180.0 * M_PI)
+    }
+    
+    func drawCursor(value : Double, len : CGFloat, width : CGFloat, color : UIColor, center : CGPoint ){
+        
+        // OK now draw cursor
+        let bz = UIBezierPath()
+        
+        color.setStroke()
+        bz.lineWidth = width
+        
+        let ang = d2R(value)
+        
+        let pt = CGPoint(x: center.x + len * CGFloat(cos(ang)), y: center.y + len * CGFloat(sin(ang)))
+        
+        bz.moveToPoint(center)
+        bz.addLineToPoint(pt)
+        
+        bz.stroke()
+    }
+    
+    func drawArc(start : Double, end : Double, r : CGFloat, width : CGFloat, color : UIColor, center : CGPoint){
+        let bz = UIBezierPath()
+        
+        color.setStroke()
+        bz.lineWidth = width * r
+        
+        let startAng = d2R(start)
+        let endAng = d2R(end)
+        
+        bz.addArcWithCenter(center, radius: r * (1.0-width), startAngle:startAng, endAngle:endAng, clockwise: true)
+        
+        bz.stroke()
+        
+    }
+    
+    
+    
+    override func drawRect(rect: CGRect) {
+        
+        // Get graphics context
+        let aContext = UIGraphicsGetCurrentContext()
+        
+        CGContextSaveGState(aContext)
+        
+        // Get the radius
+        
+        let r : CGFloat = (min(self.bounds.width, self.bounds.height) / 2.0)-1.0
+        let l  : CGFloat = r * startEndLength
+        
+        
+        let center = CGPoint(x: self.bounds.width/2.0, y:self.bounds.height/2.0)
+        
+        // Draw sphere
+        
+        sphereColor.setStroke()
+        var bz = UIBezierPath()
+        bz.lineWidth = sphereWidth
+        
+        bz.addArcWithCenter(center, radius: r, startAngle:0.0, endAngle:CGFloat(2.0 * M_PI), clockwise: true)
+        
+        bz.stroke()
+        
+        // Now put the ticks that show just top and bottom
+        
+        bz = UIBezierPath()
+        bz.moveToPoint(CGPoint(x: center.x - r + l, y:center.y))
+        bz.addLineToPoint(CGPoint(x: center.x - r, y:center.y))
+        bz.stroke()
+        
+        bz = UIBezierPath()
+        bz.moveToPoint(CGPoint(x: center.x + r - l, y:center.y))
+        bz.addLineToPoint(CGPoint(x: center.x + r, y:center.y))
+        bz.stroke()
+        
+        // Now we draw the lines :
+        
+        // Value line
+        
+        UIColor.greenColor().setStroke()
+        
+        bz = UIBezierPath()
+        var a = -d2R((value * scale) )
+        bz.moveToPoint(CGPoint(x: center.x + r * cos(a), y:center.y + r * sin(a)))
+        bz.addLineToPoint(CGPoint(x: center.x - r * cos(a), y:center.y - r * sin(a)))
+        bz.stroke()
+        
+        
+        UIColor.redColor().setStroke()
+        
+        bz = UIBezierPath()
+        a = -d2R((minValue * scale) )
+        bz.moveToPoint(CGPoint(x: center.x + r * cos(a), y:center.y + r * sin(a)))
+        bz.addLineToPoint(CGPoint(x: center.x - r * cos(a), y:center.y - r * sin(a)))
+        bz.stroke()
+        
+        bz = UIBezierPath()
+        a = -d2R((maxValue * scale))
+        bz.moveToPoint(CGPoint(x: center.x + r * cos(a), y:center.y + r * sin(a)))
+        bz.addLineToPoint(CGPoint(x: center.x - r * cos(a), y:center.y - r * sin(a)))
+        bz.stroke()
+        
+        
+        
+        CGContextRestoreGState(aContext)
+        
+        // Drawing code
+        
+        super.drawRect(rect)
+    }
+    
+    func updateData(value : Double, minValue : Double, maxValue : Double, scale : Double){
+        
+        self.value = value
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.scale = scale
+        
+        self.valueLabel.text = String(format: "%0.1f", value)
+        self.minLabel.text = String(format: "%0.1f", minValue)
+        self.maxLabel.text = String(format: "%0.1f", maxValue)
+        
+        
+        self.setNeedsDisplay()
+        
+    }
+    
+}
+
+
