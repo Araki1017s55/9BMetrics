b444b116d18efb24af5fca90d36d6d7c21003567
Paco Gorina <fgorina@gmail.com>
2016-05-08 21:34:06 +0200

    Notification names must be here

---
+++ 9BMetrics/Notifications.swift
@@ -0,0 +1,11 @@
+//
+//  Notifications.swift
+//  9BMetrics
+//
+//  Created by Francisco Gorina Vanrell on 28/4/16.
+//  Copyright © 2016 Paco Gorina. All rights reserved.
+//
+
+import Foundation
+
+let kWheelVariableChangedNotification = "wheelVariableChanged"
